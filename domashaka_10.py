9269ec274d9dd245ef25db7f6d7afc16357f7f14
Vesnani <132687769+Vesnani@users.noreply.github.com>
2023-06-11 14:48:05 +0300

    Update domashaka_10.py

--- domashaka_10.py
+++ domashaka_10.py
@@ -1,17 +1,5 @@
-from collections import UserDict
-
-
 class Field:
     def __init__(self, value=None):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
-
-    def __repr__(self):
-        return str(self)
-
-    def edit(self, value):
         self.value = value
 
 
@@ -33,27 +21,38 @@ class Phone(Field):
 class Record:
     def __init__(self, name):
         self.name = Name(name)
-        self.phones =  [Phone(phone_1)]
+        self.phones = [Phone()]
 
     def add_phone(self, phone):
-        self.phones.add_phone(phone)
+        for phone_index in self.phones:
+            phone_index.add_phone(phone)
 
     def remove_phone(self, phone):
-        self.phones.remove_phone(phone)
+        for phone_index in self.phones:
+            phone_index.remove_phone(phone)
 
     def edit_name(self, name):
         self.name.edit(name)
 
     def __str__(self):
-        return f"Name: {self.name}\nPhones: {self.phones}"
+        phone_numbers = ', '.join(str(phone) for phone in self.phones[0].value)
+        return f"Name: {self.name}\nPhones: {phone_numbers}"
 
 
-class AddressBook(UserDict):
+class AddressBook:
+    def __init__(self):
+        self.data = {}
+
     def add_record(self, record):
-        self.data[record.name.value] = record
+        if record.name.value in self.data:
+            existing_record = self.data[record.name.value]
+            existing_record.add_phone(record.phones[0].value[0])
+        else:
+            self.data[record.name.value] = record
 
     def delete_record(self, name):
-        del self.data[name]
+        if name in self.data:
+            del self.data[name]
 
     def search_records(self, search_term):
         results = []
@@ -66,32 +65,33 @@ class AddressBook(UserDict):
 def hello_func():
     return 'How can I help you?'
 
+
 def exit_func():
     return 'Good bye!'
 
-def add_func(address_book):
-    name = input('Enter name: ')
-    phone = input('Enter phone: ')
+
+def add_func(address_book, name, phone):
     record = Record(name)
     record.add_phone(phone)
     address_book.add_record(record)
     return f'You added a new contact: {name}: {phone}.'
 
-def change_func(address_book):
-    name = input('Enter name: ')
-    phone = input('Enter new phone: ')
+
+def change_func(address_book, name, phone):
     if name in address_book.data:
         record = address_book.data[name]
-        record.remove_phone(record.phones.value)
-        record.add_phone(phone)
+        record.phones[0].value = [phone]  # Замінюємо список номерів на новий список з одним номером
         return f'You changed the number to {phone} for {name}.'
     return 'Use the add command, please.'
+
 
 def show_func(address_book):
     contacts_list = ''
     for record in address_book.data.values():
-        contacts_list += f'{record.name.value} : {record.phones} \n'
+        phone_numbers = ', '.join(str(phone) for phone in record.phones[0].value)
+        contacts_list += f'{record.name.value} : [{phone_numbers}]\n'
     return contacts_list
+
 
 def main():
     address_book = AddressBook()
@@ -101,20 +101,20 @@ def main():
         'exit': exit_func,
         'close': exit_func,
         'good bye': exit_func,
-        'add': lambda: add_func(address_book),
-        'change': lambda: change_func(address_book),
+        'add': lambda: add_func(address_book, input('Enter name: '), input('Enter phone: ')),
+        'change': lambda: change_func(address_book, input('Enter name you want to change: '), input('Enter new phone: ')),
         'show all': lambda: show_func(address_book),
-        # 'phone': lambda: search_func(address_book)
     }
 
     while True:
         command = input('Enter command: ').lower()
-        if command == 'exit' or command == 'close' or command == 'good bye':
+        if command in ('exit', 'close', 'good bye'):
             print('Good bye!')
             break
 
         result = COMMANDS.get(command, lambda: 'Unknown command!')()
         print(result)
+
 
 if __name__ == '__main__':
     main()
